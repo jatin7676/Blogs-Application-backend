9386fe7cbaeff48eb82ffd6cd7ceabf635aaa81a
jatin yadav <130964755+jatin7676@users.noreply.github.com>
2025-11-07 21:32:17 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000,https://blog-app-fr.netlify.app',
+    origin: 'https://blogs-application-frontend-ggrspyl0l.vercel.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
