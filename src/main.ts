0f2308f7ab99186ba6ecb74f155f8172441471d2
jatin7676 <yadav2002jatin@gmail.com>
2025-11-07 23:54:24 +0530

    fix cors erorr

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{ cors: false});
   app.enableCors({
-    origin: 'https://blogs-application-frontend-ggrspyl0l.vercel.app/',
+    origin: ['https://blogs-application-frontend-ggrspyl0l.vercel.app', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
@@ -13,3 +13,4 @@ async function bootstrap() {
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
+//https://blogs-application-frontend-ggrspyl0l.vercel.app/
