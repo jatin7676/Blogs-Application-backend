c835536caa54a9f8e71ef7829f00f658c1e7dcf1
jatin yadav <130964755+jatin7676@users.noreply.github.com>
2025-11-08 00:31:20 +0530

    Update CORS origin to include regex for Vercel apps

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   
   const app = await NestFactory.create(AppModule,{ cors: false});
   app.enableCors({
-    origin: ['https://blogs-application-frontend.vercel.app', 'http://localhost:3000'],
+    origin: [ /\.vercel\.app$/,'https://blogs-application-frontend.vercel.app', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
